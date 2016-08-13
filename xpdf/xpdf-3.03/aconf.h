b60ac4109a93e1e0b5e5eecef86d41781a883ec8
Kuba Ober <kuba@bertec.com>
2016-08-13 15:40:21 -0400

    Remove the obsolete xpdf-3.04.

--- xpdf/xpdf-3.03/aconf.h
+++
@@ -1,2 +0,0 @@
-// This is a default that can be overwritten by configure
-#include "aconf.h.in"
--- xpdf/xpdf-3.03/xpdf/ICSupport.h
+++
@@ -1,9 +0,0 @@
-#ifndef ICSUPPORT_H
-#define ICSUPPORT_H
-
-/// FIXME Why do we need this?
-/// Supposedly, this sets type/creator of MacOS files
-#define ICS_MapRefNumAndAssign(dummy)
-
-#endif
-
